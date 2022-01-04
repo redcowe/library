3c1f26a4b088f50d29a4e1b118ba780bb60c7b67
redcowe <joshuacowell@hotmail.com>
2022-01-04 13:53:31 -0500

    Added object creating functionality

--- library.js
+++ library.js
@@ -1,28 +1,83 @@
 
-
+// DOM element declairations 
 const libraryContainerUpper = document.getElementById("libraryContainerUpper")
 const newBookButton = document.getElementById("newBookButton")
+const bookFormSubmit = document.getElementById("bookFormSubmit")
+const bookFormReadStatus = document.getElementById("bookFormReadStatus")
+const bookFormPages = document.getElementById("bookFormPages")
+const bookFormAuthor = document.getElementById("bookFormAuthor")
+const bookFormTitle = document.getElementById("bookFormTitle")  
+const createBookModalContainer = document.getElementById("createBookModalContainer")
+const createBookForm = document.getElementById("createBookForm")
 
+let myBook
 let myLibrary = []
 
-function Book(title, pages, author, isRead){
-    this.title = title
-    this.pages = pages
-    this.author = author
-    this.isRead = isRead
+class Book {
+    //Empty Constructor
+    constructor() {
+        this.title = ""
+        this.pages = 0
+        this.author = ""
+        this.isRead = false
+    }
+
+    //Setters
+    setTitle(title) {
+        this.title = title
+    }
+
+    setPages(pages) {
+        this.pages = pages
+    }
+
+    setAuthor(author) {
+        this.author = author
+    }
+
+    setReadStatus(status) {
+        this.isRead = status
+    }
+
+    //Getters 
+    getTitle() {
+        return this.title
+    }
+
+    getPages() {
+        return this.pages
+    }
+
+    getAuthor() {
+        return this.author
+    }
+
+    getReadStatus() {
+        return this.isRead
+    }
 }
 
-function createBook(myBook) {
+function createBook() {
     myBook = new Book
-    myBook.title = prompt("Enter a title:")
-    myBook.author = prompt("Enter an author:")
-    myBook.pages = prompt("Enter the number of pages:")
-    myBook.isRead = prompt("Have you read the book?")
+    myBook.setTitle(bookFormTitle.value)
+    myBook.setAuthor(bookFormAuthor.value)
+    myBook.setPages(bookFormPages.value)
+    if (bookFormReadStatus.checked) {
+        myBook.setReadStatus(true)
+    } else {
+        myBook.setReadStatus(false)
+    }
     myLibrary.push(myBook)
+    console.log(myLibrary);
+    createBookModalContainer.style.display = "none"
+    createBookForm.reset()
 }
-
-harryPotter = new Book("Harry Potter", 300, "JK Rowling", true)
+// Creating the book object 
+bookFormSubmit.addEventListener('click', () => {
+    createBook()
+}) 
 
 newBookButton.addEventListener('click', () => {
-    createBook()
+    createBookModalContainer.style.display = "flex"
 })
+
